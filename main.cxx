cc43b3d8277cea82cfc9e9e64d7719601a3ecc99
Chris Sherlock <chris.sherlock79@gmail.com>
2013-05-27 21:25:06 +1000

    Ignore build directory

--- main.cxx
+++ main.cxx
@@ -8,7 +8,8 @@ using namespace std;
 
 int main () {
     ifstream emfFile;
-    emfFile.open("test/image5.emf", ios::binary);
+    // TODO command line options needed to point to a different file
+    emfFile.open("../test/image5.emf", ios::binary);
     
     ProcessEMFHeader(emfFile);
     return 0;
